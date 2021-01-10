39346a6ec3a9354fc8e08709f96bf10f5eb80f96
Roberto Batty <texas_telecaster@hotmail.com>
2021-01-10 11:10:39 -0500

    test api dev

---
+++ server/src/index.js
@@ -0,0 +1,34 @@
+﻿/*
+ *
+ * PACKAGES
+ *
+ */
+const express = require('express');
+const { json, urlencoded } = express;
+const app = express();
+const morgan = require('morgan');
+const ewelink = require('ewelink-api');
+const cors = require('cors');
+
+/*
+ *
+ * USE
+ *
+ */
+app.use(morgan('dev'));
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+/**
+ *
+ * <> ROUTES
+ *
+ */
+require('./routes/routes')(app);
+
+app.listen(process.env.PORT || 4230, () => {
+  console.log('connected');
+});
+
+module.exports = app;
