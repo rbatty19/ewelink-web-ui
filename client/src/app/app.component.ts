4411e3e608f9240bb9468d5ce3705b5009885920
Roberto Batty <texas_telecaster@hotmail.com>
2021-01-10 21:16:32 -0500

    svg logo fix name

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
 
     this.themeService.initTheme();
     this.matIconRegistry.addSvgIcon(
-      "logo",
+      "github",
       this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.flaticon.es/svg/static/icons/svg/25/25231.svg'));
 
   }
