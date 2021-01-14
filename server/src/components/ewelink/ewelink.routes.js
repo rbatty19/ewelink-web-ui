7c4a7b81280df34b5d7ffd56aff32f5764595139
Roberto Batty <texas_telecaster@hotmail.com>
2021-01-14 10:08:00 -0500

    GetDevicesValidator on

--- server/src/components/ewelink/ewelink.routes.js
+++ server/src/components/ewelink/ewelink.routes.js
@@ -8,10 +8,6 @@ const { LoginValidator, GetDevicesValidator, GetDeviceValidator } = require('./e
 
 module.exports = (app) => {
   app.post('/login', LoginValidator, Login);
-  app.post(
-    '/devices',
-    //  GetDevicesValidator,
-    GetDevices,
-  );
+  app.post('/devices', GetDevicesValidator, GetDevices);
   app.post('/device', GetDeviceValidator, GetDevice);
 };
